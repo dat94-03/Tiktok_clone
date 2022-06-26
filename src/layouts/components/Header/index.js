3dc83e49234ef73d96fa3fab3eaf3b1679eb1af0
dat94-03 <tiendat942003@gmail.com>
2022-06-26 10:49:48 +0700

    3

--- src/layouts/components/Header/index.js
+++ src/layouts/components/Header/index.js
@@ -22,7 +22,7 @@ import Menu from '~/components/Popper/Menu';
 import { InboxIcon, MessageIcon, UploadIcon } from '~/components/Icons';
 import Image from '~/components/Image';
 import Search from '../Search';
-import routesConfig from '~/config/routes';
+import config from '~/config';
 
 const cx = classNames.bind(styles);
 
@@ -98,7 +98,7 @@ function Header() {
   return (
     <header className={cx('wrapper')}>
       <div className={cx('inner')}>
-        <Link to={routesConfig.home} className={cx('logo-name')}>
+        <Link to={config.routes.home} className={cx('logo-name')}>
           <img src={images.logo} alt="Tiktok" />
         </Link>
 
--- src/utils/httpRequest.js
+++ src/utils/httpRequest.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 const request = axios.create({
-    baseURL: 'https://tiktok.fullstack.edu.vn/api/',
+  baseURL: process.env.REACT_APP_BASE_URL,
 });
 export const get = async (apiPath, option = {}) => {
-    const response = await request.get(apiPath, option);
-    return response.data;
+  const response = await request.get(apiPath, option);
+  return response.data;
 };
 export default request;
